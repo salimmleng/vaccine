047504970e9ee4c1ed28d42733aebf4294064088
Salim Miah <salimmiah0785@gmail.com>
2024-10-19 01:44:40 -0700

    done

--- immunohub/settings.py
+++ immunohub/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt',
     'rest_framework.authtoken',
     'dj_rest_auth',
     'accounts',
